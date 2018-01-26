5841ee3f11f71432c98437a2859c80365cb62f6d
MIMEK\Mati <matib16@gmail.com>
2018-01-26 14:43:41 +0100

    poprawa widoków, ustawienie poprawnych składników przy każdej pizzy w order.html z JSONa

--- src/main/resources/static/wait.js
+++ src/main/resources/static/wait.js
@@ -1,22 +1,22 @@
-	setTimeout(
-	  function() 
-	{
+setTimeout(
+    function()
+    {
 
 
-		document.getElementById("order-txt").innerHTML = "You order is being prepared!";
-		document.getElementById("order-txt2").innerHTML = "Delivery time: ~30min";
-		document.getElementById("img-load").src="success.png";
-	  }, 5000);
+        document.getElementById("order-txt").innerHTML = "You order is being prepared!";
+        document.getElementById("order-txt2").innerHTML = "Delivery time: ~30min";
+        document.getElementById("img-load").src="success.png";
+    }, 5000);
 
 
-   $(document).ready(function() {
+$(document).ready(function() {
     $.ajax({
         url: "https://reqres.in/api/users/2"
     }).then(function(data) {
-       $('.greeting-id').append(data.data.id);
-	   
-		$('.greeting-content').append(data.data.last_name);
-	   
+        $('.greeting-id').append(data.data.id);
+
+        $('.greeting-content').append(data.data.last_name);
+
     });
 });
 
